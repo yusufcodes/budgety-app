da6a1d6004637ec08bbe2bd6c817e8e15e460928
yusufcodes <yusufcodes@outlook.com>
2018-12-09 13:57:20 +0000

    Added code to UI Controller to retrieve user inputs, store these and make them accessible to the main controller.

--- app.js
+++ app.js
@@ -6,16 +6,48 @@ var budgetController = (function() {
 // UI module
 var UIController = (function() {
 
+    var DOMstrings = 
+    {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function()
+        {
+            /* Getting the input values
+            Returned as an object with the three properties we need */
+            return {
+                // Income type - 'inc' or 'exp'
+                type: document.querySelector(DOMstrings.inputType).value,
+                // Description
+                desc: document.querySelector(DOMstrings.inputDescription).value,
+                // Amount
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function()
+        {
+            return DOMstrings;
+        }
+
+    }
+
 })();
 
 // Main module
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
     // Income / Expense should be added to the relevant area
     var ctrlAddItem = function ()
     {
         // 1. Get the input data from fields
-
+        var input = UICtrl.getInput();
+        
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
@@ -23,11 +55,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget
-        console.log("it works!");
+        console.log(input);
 
     }
     // Event listener for when the user clicks on the 'add' button
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     // Event listener for when the user clicks *any* key
     document.addEventListener('keypress', function(event) 
