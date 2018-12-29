6205d6bdadd6858020fb430f1f9ab82d0f388741
yusufcodes <yusufcodes@outlook.com>
2018-12-29 07:47:48 +0000

    Completed code to add new item to UI. HTML string is made and edited using the user's entered information, and then placed into the DOM using insertAdjacentHTML.

--- app.js
+++ app.js
@@ -71,7 +71,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -90,24 +92,42 @@ var UIController = (function() {
         },
 
         // Adding an income / expense to the user interface
-        /* obj: item to add, type: the type of income being added */
+        /* obj: item to add
+           type: the type of income being added */
         addListItem: function(obj, type)
         {
-            var html, newHtml;
-            
+            var html, newHtml, element;
 
             /* My method: use 'obj' attributes to create a new HTML string:
             var HTMLstring = '<div class="item clearfix" id='+type+'-'+obj.id+'"><div class="item__description">'+obj.desc+'</div><div class="right clearfix"><div class="item__value">+'+obj.value+'</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>' */
 
             /* Instructor method: Use if/else to determine income type, then replace the placeholders using 'replace' method */
+
             // Create HTML string
             if (type === 'inc')
-            {}
+            {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                element = DOMstrings.incomeContainer;
+            }
+            else if (type === 'exp')
+            {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                element = DOMstrings.expensesContainer;
+            }
 
+            // Place data into 'new' HTML string - using replace
+            /* How 'replace' works: 
+            Placeholders are represented as follows: %id%, %description%, %value%.
+            This is passed into 'replace' each time, followed by what we want to replace it with. */
 
-            // Place data into HTML string
+            newHtml = html.replace('%id%', obj.id); // This copies 'html' into 'newHtml', with the object's id
+
+            /* Following two lines uses the replace method on newHtml now, since we have already copied the original HTML into newHtml. */
+            newHtml = newHtml.replace('%description%', obj.desc);
+            newHtml = newHtml.replace('%value%', obj.value);
 
             // Insert HTML into DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         getDOMstrings: function()
@@ -148,6 +168,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var addItem = budgetCtrl.addItem(input.type, input.desc, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(addItem, input.type);
 
         // 4. Calculate the budget
 
