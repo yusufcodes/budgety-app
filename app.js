6fdcea2e919276c4b689d04e8ebb03060d1044da
yusufcodes <yusufcodes@outlook.com>
2018-12-29 08:00:26 +0000

    Completed section on clearing the input fields, using: -querySelectorAll -forEach

--- app.js
+++ app.js
@@ -130,6 +130,16 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function()
+        {
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fields.forEach( function(current, index, array)
+            {
+                current.value = "";
+            });
+        },
+
         getDOMstrings: function()
         {
             return DOMstrings;
@@ -170,9 +180,12 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
         UICtrl.addListItem(addItem, input.type);
 
-        // 4. Calculate the budget
+        // 4. Clear fields
+        UICtrl.clearFields();
 
-        // 5. Display the budget
+        // 5. Calculate the budget
+
+        // 6. Display the budget
     }
 
     return {
